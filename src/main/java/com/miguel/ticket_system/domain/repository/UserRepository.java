19ec651416037074a8225cbf3af34eff45954af3
Miguel-Frnds <miguel.pereiraf05@gmail.com>
2026-04-12 20:55:02 -0300

    Cria repository inicial de User

---
+++ src/main/java/com/miguel/ticket_system/domain/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.miguel.ticket_system.domain.repository;
+
+import com.miguel.ticket_system.domain.model.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+    boolean existsByUsername(String username);
+    boolean existsByEmail(String email);
+    boolean existsByUsernameAndIdNot(String username, Long id);
+    boolean existsByEmailAndIdNot(String email, Long id);
+}
